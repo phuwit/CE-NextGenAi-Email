25ba94e84cf9d7048b04120914545c9082c9b95a
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-03-27 23:50:52 +0700

    reset and verify template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [
-    require('tailwindcss-preset-email'),
+    require('tailwindcss-preset-email')
   ],
   content: [
     './components/**/*.html',
     './emails/**/*.html',
     './layouts/**/*.html',
   ],
+  theme: {
+    extend: {
+      colors: {
+        "light-purple": "#F6E6FF",
+        "dark-purple": "#9A46FB",
+      },
+    },
+  },
 }
